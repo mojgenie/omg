55cc546e81bab8e37ad1ff5e9c0f024a3b98e876
dheeraj-mojgenie <dheeraj@mojgenie.com>
2023-12-18 17:33:08 +0530

    commit

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -68,7 +68,7 @@ function Form({ id, updateUser, name, email, phone,ValidateEmail,ValidatePhone,f
                             htmlFor="floating_phone"
                             className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                         >
-                            Phone number (808-456-7890)
+                            Phone number (8084567890)
                         </label>
                         {phone ? ValidatePhone(phone)?'':flag?<p className="text-red-500 text-xs mt-1">Not a valid phone</p>:'':flag?<p className="text-red-500 text-xs mt-1">Phone required</p>:'' }
                     </div>
